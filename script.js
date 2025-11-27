1d83468d7d4c9d9c2fbfeb600aa27c60663e7c7a
nikilhadinesh <nikilhadinesh@gmail.com>
2025-11-27 18:22:40 +0530

    Changes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const themeToggle = document.querySelector("#theme-toggle-btn");
 
 
 // API setup
-const API_KEY="AIzaSyAxDH7sP0E3p91J8LmpZUukRwuNtuUcsyU";
+const API_KEY="AIzaSyDEhuPaBXzGfo-Nu76cDgUlAYgsxCm6yCk";
 const API_URL=`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?
 key=${API_KEY}`;
 
